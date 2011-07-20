22605bb55486841668b74c2ff59f03bff2411491
Nick Howes <n.howes@warwick.ac.uk>
2011-07-20 12:36:05 +0100

    Embedding a small Android project and test project.
    Up until now I've been linking the build library into
    an external project to test it, but it makes more sense
    to have a minimal project that includes the Java JNI class.
    It may even make sense for the project to generate a self
    contained .jar, to make it easier to drop into a project
    that needs FFMPEG.

---
+++ ProjectTest/src/uk/co/halfninja/videokit/MainActivityTest.java
@@ -0,0 +1,21 @@
+package uk.co.halfninja.videokit;
+
+import android.test.ActivityInstrumentationTestCase2;
+
+/**
+ * This is a simple framework for a test of an Application.  See
+ * {@link android.test.ApplicationTestCase ApplicationTestCase} for more information on
+ * how to write and extend Application tests.
+ * <p/>
+ * To run this test, you can type:
+ * adb shell am instrument -w \
+ * -e class uk.co.halfninja.videokit.MainActivityTest \
+ * uk.co.halfninja.videokit.tests/android.test.InstrumentationTestRunner
+ */
+public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActivity> {
+
+    public MainActivityTest() {
+        super("uk.co.halfninja.videokit", MainActivity.class);
+    }
+
+}
