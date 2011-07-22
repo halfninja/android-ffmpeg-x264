873fa2e172da0f9f36ace9e782f3f75e5134d255
Nick Howes <nick@nickhowes.co.uk>
2011-07-22 07:56:20 +0100

    Build the ffmpeg executable, for science

--- Project/jni/Android.mk
+++ Project/jni/Android.mk
@@ -17,3 +17,19 @@ LOCAL_LDLIBS += -llog -lz $(FFMPEG_LIBS) x264/libx264.a
 LOCAL_SRC_FILES := videokit/uk_co_halfninja_videokit_Videokit.c videokit/ffmpeg.c videokit/cmdutils.c
 include $(BUILD_SHARED_LIBRARY)
 
+
+include $(CLEAR_VARS)
+LOCAL_MODULE  := ffmpeg
+FFMPEG_LIBS := $(addprefix ffmpeg/, \
+ libavdevice/libavdevice.a \
+ libavformat/libavformat.a \
+ libavcodec/libavcodec.a \
+ libavfilter/libavfilter.a \
+ libswscale/libswscale.a \
+ libavutil/libavutil.a \
+ libpostproc/libpostproc.a )
+LOCAL_CFLAGS += -g -Iffmpeg -Ivideokit -Wno-deprecated-declarations 
+LOCAL_LDLIBS += -llog -lz $(FFMPEG_LIBS) x264/libx264.a
+LOCAL_SRC_FILES := ffmpeg/ffmpeg.c ffmpeg/cmdutils.c
+include $(BUILD_EXECUTABLE)
+
